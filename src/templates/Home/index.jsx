884e1af52c9bff50db85a8d83f2e85b2e10eb389
zailua <gabrielmmc1@gmail.com>
2022-11-25 14:31:01 -0300

    newSave

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button } from '../../components/Button'
 import { Posts } from '../../components/Posts'
 import { loadPosts } from '../../utils/load-posts'
 import './styles.css'
@@ -7,15 +8,27 @@ import './styles.css'
 export class Home extends Component {
 	state = {
 		posts: [],
+		allPosts: [],
+		page: 0,
+		postsPerPage: 2,
 	}
 
-	 componentDidMount() {
-		 this.loadPosts()
+	componentDidMount() {
+		this.loadPosts()
 	}
 
 	loadPosts = async () => {
-    const postsAndPhotos = await loadPosts()
-		this.setState({ posts: postsAndPhotos })
+		const { page, postsPerPage } = this.state
+
+		const postsAndPhotos = await loadPosts()
+		this.setState({
+			posts: postsAndPhotos.slice(page, postsPerPage),
+			allPosts: postsAndPhotos,
+		})
+	}
+
+	loadMorePosts = () => {
+		console.log('load more posts')
 	}
 
 	render() {
@@ -24,6 +37,7 @@ export class Home extends Component {
 		return (
 			<div className="container">
 				<Posts posts={posts} />
+				<Button text="Load" event={this.loadMorePosts}/>
 			</div>
 		)
 	}
